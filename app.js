49e9b9763b1cd99d2aa33bd3b4e9f6fffc95c9ca
ajay kumar <ajay8704276@gmail.com>
2020-05-28 18:58:08 +0530

    implementing cors - cross origin resource sharing

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -26,6 +27,25 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 // 1) GLOBAL MIDDLEWARES
+
+//implement cors - cross origin request sharing
+app.use(cors());
+//Access control Allow origin * for all the request
+//Allowing the user to access the api
+// if we want to allow only specific routes
+//then in that case will pass cors() as middleware to
+//that api routes
+
+//api.natours.com ,  front end natours.com
+/*app.use(
+  cors({
+    origin: 'https://www.api.natours.com' // allowing this routes to access the api
+  })
+);*/
+
+app.options('*', cors());
+//app.options('/api/v1/tours/:id', cors());
+
 // Serving static files
 app.use(express.static(path.join(__dirname, 'public')));
 
